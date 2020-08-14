c9fee180e3d9134dc307d92ded522c8873df89e2
DarinkaTownsend <66121094+DarinkaTownsend@users.noreply.github.com>
2020-08-13 22:31:21 -0500

    Solución Code Smell Duplicate Code

--- src/modelos/Profesor.java
+++ src/modelos/Profesor.java
@@ -4,19 +4,14 @@ import java.util.ArrayList;
 
 public class Profesor {
     public String codigo;
-    public String nombre;
-    public String apellido;
-    public int edad;
+    Persona persona;
     public String direccion;
     public String telefono;
     public InformacionAdicionalProfesor info;
     public ArrayList<Paralelo> paralelos;
 
-    public Profesor(String codigo, String nombre, String apellido, String facultad, int edad, String direccion, String telefono) {
-        this.codigo = codigo;
-        this.nombre = nombre;
-        this.apellido = apellido;
-        this.edad = edad;
+    public Profesor(String codigo, String facultad,String direccion, String telefono, Persona persona) {
+        this.codigo = codigo;        
         this.direccion = direccion;
         this.telefono = telefono;
         paralelos= new ArrayList<>();

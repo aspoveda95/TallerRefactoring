dc36b44c5e3f86bbeb72fdbdb40e9910133a19e2
sbenalca <37081244+sbenalca@users.noreply.github.com>
2020-08-13 23:06:49 -0500

    Refactor Safely Delete y Refactor en clase Profesor

--- src/modelos/Profesor.java
+++ src/modelos/Profesor.java
@@ -2,22 +2,43 @@ package modelos;
 
 import java.util.ArrayList;
 
-public class Profesor {
-    public String codigo;
-    Persona persona;
-    
-    public InformacionAdicionalProfesor info;
-    public ArrayList<Paralelo> paralelos;
+public class Profesor extends Persona{
+    protected String codigo;
+    protected int añosdeTrabajo;
+    protected String facultad;
+    protected double BonoFijo;
+    protected ArrayList<Paralelo> paralelos;
 
-    public Profesor(String codigo, String facultad,String direccion, String telefono, Persona persona) {
-        this.codigo = codigo;    
-        
-        paralelos= new ArrayList<>();
+    public Profesor(String nombre, String apellido, int edad, String direccion, String telefono) {
+        super(nombre, apellido, edad, direccion, telefono);
     }
+
+    public Profesor(String codigo, ArrayList<Paralelo> paralelos, String nombre, String apellido, int edad, String direccion, String telefono) {
+        super(nombre, apellido, edad, direccion, telefono);
+        this.codigo = codigo;
+        this.paralelos = paralelos;
+    }
+
+    public Profesor(String codigo, int añosdeTrabajo, String facultad, double BonoFijo, ArrayList<Paralelo> paralelos, String nombre, String apellido, int edad, String direccion, String telefono) {
+        super(nombre, apellido, edad, direccion, telefono);
+        this.codigo = codigo;
+        this.añosdeTrabajo = añosdeTrabajo;
+        this.facultad = facultad;
+        this.BonoFijo = BonoFijo;
+        this.paralelos = paralelos;
+    }
+    
+
+    
+     
     
     public void anadirParalelos(Paralelo p){
         paralelos.add(p);
     }
     
-    
+    public double calcularSueldo(Profesor prof){
+        double sueldo=0;
+        sueldo= prof.añosdeTrabajo*600 + prof.BonoFijo;
+        return sueldo;
+    }  
 }
